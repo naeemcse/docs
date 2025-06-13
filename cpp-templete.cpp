a872bb44bb36dd80640b6da00f7d90d098ce4bf8
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2025-06-13 13:09:50 +0600

    Cpp templete is written

---
+++ cpp-templete.cpp
@@ -0,0 +1,43 @@
+
+//ALHAMDULILLAH
+//Solved by Najmul Islam Naeem
+#define   ll            long long int
+#define   nn            cin>>n
+#define   ss            string s;cin>>s
+#define   vs            vector<string>v(n)
+#define   vi            vector<ll>v(n)
+#define   vl            vector<ll>
+#define   cinv          for(ll i=0;i<n;i++) cin>>v[i];
+#define   coutv         for(ll i=0;i<n;i++)cout<<v[i]<<" ";cout<<endl;
+#define   srt(v)        sort(v.begin(),v.end());
+#define   rvs(v)        reverse(v.begin(),v.end());
+#define   gsrt(v)       sort(v.begin(), v.end(), greater<ll>());
+#define   cin(a)          for(ll i=0;i<n;i++) cin>>a[i];
+#define  cout(a)          for(ll i=0;i<n;i++) cout<<a[i]<<" ";cout<<endl;
+#define   srta          sort(a,a+n);
+#define   gsrta         sort(a,a+n,greater<ll>());
+#define  yes            cout<<"YES"<<endl;
+#define  no             cout<<"NO"<<endl;
+#define   fr            for(ll i=0;i<n;i++)
+#define  nl             cout<<"\n";
+#define  vprint   for (auto x : v) cout << x << " ";
+#define mx(a)       *max_element(a.begin(), a.end());
+#define mn(a)       *min_element(a.begin(), a.end());
+//#define   sum(a)               accumulate(a.begin(),a.end(),0LL)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ll t;cin>>t;while(t--)
+    {
+         ll n, m, k, a, b, cnt=0, mx=0, mn = INT_MAX, sum=0;
+
+
+
+
+
+    }
+}
+
+
